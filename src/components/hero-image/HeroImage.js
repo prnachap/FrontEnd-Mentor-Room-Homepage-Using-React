7983bbccb44b22fbe12153868e4b91016283d04a
prnachap <33637318+prnachap@users.noreply.github.com>
2021-01-30 21:54:55 +0530

    Update HeroImage.js
    Alt attribute removed from Source Element

--- src/components/hero-image/HeroImage.js
+++ src/components/hero-image/HeroImage.js
@@ -4,7 +4,7 @@ import "./hero-image.style.scss";
 const HeroImage = ({ mobile, desktop, alt, position }) => {
   return (
     <picture className={`hero__image ${position}`}>
-      <source srcSet={mobile} media="(max-width:37.5em)" alt={alt} />
+      <source srcSet={mobile} media="(max-width:37.5em)"/>
       <img src={desktop} alt={alt} />
     </picture>
   );
